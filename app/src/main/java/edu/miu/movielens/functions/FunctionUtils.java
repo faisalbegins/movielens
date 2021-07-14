dd4adae156a1be828506364efa5f1a2360013fc2
Faisal Ahmed <faisalbegins@gmail.com>
2021-07-14 15:25:17 -0500

    added more queries

--- app/src/main/java/edu/miu/movielens/functions/FunctionUtils.java
+++ app/src/main/java/edu/miu/movielens/functions/FunctionUtils.java
@@ -1,9 +1,12 @@
 package edu.miu.movielens.functions;
 
+import edu.miu.movielens.model.Genre;
 import edu.miu.movielens.model.Movie;
+import edu.miu.movielens.model.MovieGenre;
 import edu.miu.utils.PseudoDatabase;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -35,4 +38,28 @@ public interface FunctionUtils {
                     .limit(1)
                     .map(m -> m.director().name())
                     .findFirst();
+
+    // top k movies from each genre in a given year
+    BiFunction<Integer, Integer, Map<Genre, List<String>>> topKMoviesByGenreInGivenYear =
+            (k, year) -> database.getMovieGenres().stream()
+                    .collect(Collectors.groupingBy(MovieGenre::genre))
+                    .entrySet()
+                    .stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, (entity) -> FunctionUtils.sortedKMoveGenreByImdbScore.apply(entity.getValue(), k)))
+                    .entrySet()
+                    .stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, e -> FunctionUtils.toMovieNames.apply(e.getValue())));
+
+    // helper functions
+    BiFunction<List<MovieGenre>, Integer, List<MovieGenre>> sortedKMoveGenreByImdbScore =
+            (movieGenres, k) -> movieGenres.stream()
+                    .sorted((mg1, mg2) -> Double.compare(mg2.movie().imdbScore(), mg1.movie().imdbScore()))
+                    .limit(k)
+                    .collect(Collectors.toList());
+
+    Function<List<MovieGenre>, List<String>> toMovieNames =
+            (movieGenres) -> movieGenres.stream()
+                    .map(g -> g.movie().name())
+                    .collect(Collectors.toList());
+
 }
--- app/src/test/java/edu/miu/movielens/FunctionUtilsTest.java
+++ app/src/test/java/edu/miu/movielens/FunctionUtilsTest.java
@@ -1,10 +1,10 @@
 package edu.miu.movielens;
 
+import edu.miu.movielens.functions.FunctionUtils;
+import edu.miu.movielens.model.Genre;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static edu.miu.movielens.functions.FunctionUtils.mostSuccessfulDirectorInGivenYear;
 import static edu.miu.movielens.functions.FunctionUtils.topKMoviesInGivenYear;
@@ -25,5 +25,39 @@ public class FunctionUtilsTest {
         Optional<String> result = mostSuccessfulDirectorInGivenYear.apply(2015);
         assertTrue(result.isPresent());
         assertEquals(expected, result.get());
+    }
+
+    @Test
+    public void top_k_movies_from_each_genre_in_a_given_year() {
+        Map<Genre, List<String>> expected = new HashMap<>();
+        expected.put(Genre.valueOf("WAR"), Arrays.asList("Casablanca"));
+        expected.put(Genre.valueOf("ACTION"), Arrays.asList("Kickboxer: Vengeanc"));
+        expected.put(Genre.valueOf("HISTORY"), Arrays.asList("Schindler's List"));
+        expected.put(Genre.valueOf("HISTORY"), Arrays.asList("Schindler's List"));
+        expected.put(Genre.valueOf("COMEDY"), Arrays.asList("Forrest Gump"));
+        expected.put(Genre.valueOf("ADVENTURE"), Arrays.asList("The Lord of the Rings: The Return of the King"));
+        expected.put(Genre.valueOf("THRILLER"), Arrays.asList("The Dark Knight"));
+        expected.put(Genre.valueOf("ANIMATION"), Arrays.asList("Spirited Away"));
+        expected.put(Genre.valueOf("DOCUMENTARY"), Arrays.asList("Butterfly Girl"));
+        expected.put(Genre.valueOf("MUSICAL"), Arrays.asList("The Lion King"));
+        expected.put(Genre.valueOf("SCI_FI"), Arrays.asList("Star Wars: Episode V - The Empire Strikes Back"));
+        expected.put(Genre.valueOf("MYSTERY"), Arrays.asList("Se7en"));
+        expected.put(Genre.valueOf("MUSIC"), Arrays.asList("Samsara"));
+        expected.put(Genre.valueOf("FILM_NOIR"), Arrays.asList("Rebecca"));
+        expected.put(Genre.valueOf("FAMILY"), Arrays.asList("Modern Times"));
+        expected.put(Genre.valueOf("NEWS"), Arrays.asList("The Square"));
+        expected.put(Genre.valueOf("SPORT"), Arrays.asList("The Other Dream Team"));
+        expected.put(Genre.valueOf("ROMANCE"), Arrays.asList("Casablanca"));
+        expected.put(Genre.valueOf("WESTERN"), Arrays.asList("The Good the Bad and the Ugly"));
+        expected.put(Genre.valueOf("HORROR"), Arrays.asList("A Beginner's Guide to Snuff"));
+        expected.put(Genre.valueOf("BIOGRAPHY"), Arrays.asList("Schindler's List"));
+        expected.put(Genre.valueOf("SHORT"), Arrays.asList("Marilyn Hotchkiss' Ballroom Dancing and Charm School"));
+        expected.put(Genre.valueOf("FANTASY"), Arrays.asList("The Lord of the Rings: The Return of the King"));
+        expected.put(Genre.valueOf("CRIME"), Arrays.asList("The Shawshank Redemption"));
+        expected.put(Genre.valueOf("DRAMA"), Arrays.asList("The Shawshank Redemption"));
+
+        Map<Genre, List<String>> result = FunctionUtils.topKMoviesByGenreInGivenYear.apply(1, 2016);
+
+        assertEquals(expected.keySet(), result.keySet());
     }
 }
